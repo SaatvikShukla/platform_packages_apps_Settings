LineageOS/android_packages_apps_Settings

Kill off a few MSIM tab hosts.
Change-Id: Ib6ec06e5d772b5d1ebd2d8af6f89b8fc6ea2c55d

src/com/android/settings/deviceinfo/Status.java
@@ -44,6 +44,7 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -59,7 +60,6 @@ import com.android.internal.telephony.PhoneStateIntentReceiver;
 import com.android.internal.telephony.SubscriptionController;
 import com.android.internal.util.ArrayUtils;
 import com.android.settings.R;
-import com.android.settings.SelectSubscription;
 import com.android.settings.Utils;
 
 import java.lang.ref.WeakReference;
@@ -120,7 +120,7 @@ public class Status extends PreferenceActivity {
         KEY_ICC_ID
     };
 
-    private static final String BUTTON_SELECT_SUB_KEY = "button_aboutphone_msim_status";
+    private static final String BUTTON_SUBSCRIPTIONS_KEY = "button_aboutphone_msim_status";
 
     static final String CB_AREA_INFO_RECEIVED_ACTION =
             "android.cellbroadcastreceiver.CB_AREA_INFO_RECEIVED";
@@ -416,12 +416,32 @@ public class Status extends PreferenceActivity {
                 }
             });
 
-        PreferenceScreen selectSub = (PreferenceScreen) findPreference(BUTTON_SELECT_SUB_KEY);
-        if (selectSub != null) {
-            Intent intent = selectSub.getIntent();
-            intent.putExtra(SelectSubscription.PACKAGE, "com.android.settings");
-            intent.putExtra(SelectSubscription.TARGET_CLASS,
-                    "com.android.settings.deviceinfo.msim.MSimSubscriptionStatus");
+        Preference subs = findPreference(BUTTON_SUBSCRIPTIONS_KEY);
+        if (subs != null) {
+            PreferenceScreen prefSet = getPreferenceScreen();
+            int numPhones = TelephonyManager.getDefault().getPhoneCount();
+            SubscriptionManager subscriptionManager = SubscriptionManager.from(this);
+
+            for (int i = 0; i < numPhones; i++) {
+                SubscriptionInfo sir =
+                        subscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(i);
+                Preference pref = new Preference(this);
+
+                pref.setOrder(subs.getOrder());
+                pref.setTitle(getString(R.string.sim_card_status_title, i + 1));
+                if (sir != null) {
+                    pref.setSummary(sir.getDisplayName());
+                } else {
+                    pref.setSummary(R.string.sim_card_summary_empty);
+                }
+
+                Intent intent = new Intent(this, MSimSubscriptionStatus.class);
+                intent.putExtra(MSimSubscriptionStatus.EXTRA_SLOT_ID, i);
+                pref.setIntent(intent);
+
+                prefSet.addPreference(pref);
+            }
+            prefSet.removePreference(subs);
         }
     }
 

SaatvikShukla/platform_packages_apps_Settings

Conflicts:
	res/xml/device_info_subscription_status.xml
	src/com/android/settings/SecuritySettings.java
	src/com/android/settings/deviceinfo/msim/MSimSubscriptionStatus.java

src/com/android/settings/deviceinfo/Status.java
@@ -44,6 +44,7 @@ import android.telephony.PhoneNumberUtils;
 import android.telephony.PhoneStateListener;
 import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
+import android.telephony.SubscriptionInfo;
 import android.telephony.SubscriptionManager;
 import android.text.TextUtils;
 import android.view.MenuItem;
@@ -59,7 +60,6 @@ import com.android.internal.telephony.PhoneStateIntentReceiver;
 import com.android.internal.telephony.SubscriptionController;
 import com.android.internal.util.ArrayUtils;
 import com.android.settings.R;
-import com.android.settings.SelectSubscription;
 import com.android.settings.Utils;
 
 import java.lang.ref.WeakReference;
@@ -119,7 +119,7 @@ public class Status extends PreferenceActivity {
         KEY_ICC_ID
     };
 
-    private static final String BUTTON_SELECT_SUB_KEY = "button_aboutphone_msim_status";
+    private static final String BUTTON_SUBSCRIPTIONS_KEY = "button_aboutphone_msim_status";
 
     static final String CB_AREA_INFO_RECEIVED_ACTION =
             "android.cellbroadcastreceiver.CB_AREA_INFO_RECEIVED";
@@ -415,12 +415,32 @@ public class Status extends PreferenceActivity {
                 }
             });
 
-        PreferenceScreen selectSub = (PreferenceScreen) findPreference(BUTTON_SELECT_SUB_KEY);
-        if (selectSub != null) {
-            Intent intent = selectSub.getIntent();
-            intent.putExtra(SelectSubscription.PACKAGE, "com.android.settings");
-            intent.putExtra(SelectSubscription.TARGET_CLASS,
-                    "com.android.settings.deviceinfo.msim.MSimSubscriptionStatus");
+        Preference subs = findPreference(BUTTON_SUBSCRIPTIONS_KEY);
+        if (subs != null) {
+            PreferenceScreen prefSet = getPreferenceScreen();
+            int numPhones = TelephonyManager.getDefault().getPhoneCount();
+            SubscriptionManager subscriptionManager = SubscriptionManager.from(this);
+
+            for (int i = 0; i < numPhones; i++) {
+                SubscriptionInfo sir =
+                        subscriptionManager.getActiveSubscriptionInfoForSimSlotIndex(i);
+                Preference pref = new Preference(this);
+
+                pref.setOrder(subs.getOrder());
+                pref.setTitle(getString(R.string.sim_card_status_title, i + 1));
+                if (sir != null) {
+                    pref.setSummary(sir.getDisplayName());
+                } else {
+                    pref.setSummary(R.string.sim_card_summary_empty);
+                }
+
+                Intent intent = new Intent(this, SimStatus.class);
+                intent.putExtra(SimStatus.EXTRA_SLOT_ID, i);
+                pref.setIntent(intent);
+
+                prefSet.addPreference(pref);
+            }
+            prefSet.removePreference(subs);
         }
     }
 